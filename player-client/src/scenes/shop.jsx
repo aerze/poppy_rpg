5a9bea8ed07aaee94efe37e50e722a1c24cf7693
Abby <abby@mythril.co>
2024-06-23 08:41:17 +0000

    add icons, update shop

--- player-client/src/scenes/shop.jsx
+++ player-client/src/scenes/shop.jsx
@@ -2,21 +2,21 @@ import "./shop.scss";
 
 const shopItems = [
   {
-    image: '🧪',
+    image: '/icons/icon1.png',
     name: 'Potion',
     description: '+20HP',
     price: '200'
   }, {
-    image: '🥔',
+    image: '/icons/icon3.png',
     name: 'Potato',
     description: '+10HP',
-    price: 500
+    price: '500K'
   },
   {
-    image: '🎟️',
+    image: '/icons/icon31.png',
     name: 'Free Revive Ticket',
     description: 'Revives 1 Party Member',
-    price: 2000
+    price: '120M'
   }
 ]
 
@@ -25,10 +25,30 @@ const items = [...shopItems, ...shopItems, ...shopItems];
 function ShopItem({ image, name, description, price }) {
   return (
     <div className="shop-item">
-      <div className="item-image">{image}</div>
-      <div className="item-name">{name}</div>
-      <div className="item-description">{description}</div>
-      <div className="item-price">{price}◈</div>
+      <div className="item-image">
+        <div className="bg2"></div>
+        <div className="bg1"></div>
+        <div className="icon">
+          <img src={image} />
+        </div>
+      </div>
+      {/* <div className="item-name">
+        {name}
+      </div> */}
+      <div className="item-description">
+        <div className="name">{name}</div>
+        <div className="text">
+          {description}
+        </div>
+      </div>
+      <div className="item-price">
+        <div className="item-button">
+          <div className="bg"></div>
+          <div className="t">
+            {price} ◈
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
